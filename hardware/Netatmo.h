19c6b2caa22f3a42a2054d21488b1310f0010995
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2024-01-08 22:29:28 +0100

    Update Netatmo.h

--- hardware/Netatmo.h
+++ hardware/Netatmo.h
@@ -10,14 +10,7 @@ namespace Json
 
 class CNetatmo : public CDomoticzHardwareBase
 {
-      public:
-	CNetatmo(int ID, const std::string &username, const std::string &password);
-	~CNetatmo() override = default;
-
-	bool WriteToHardware(const char *, unsigned char) override;
-	void SetSetpoint(int idx, float temp);
-	bool SetProgramState(int idx, int newState);
-
+      //
       private:
 	enum _eNetatmoType
 	{
@@ -31,6 +24,7 @@ class CNetatmo : public CDomoticzHardwareBase
 		NETYPE_CAMERAS,
 		NETYPE_EVENTS
 	};
+        Json::Value m_root;
 	std::string m_clientId;
 	std::string m_clientSecret;
 	std::string m_scopes;
@@ -68,16 +62,24 @@ class CNetatmo : public CDomoticzHardwareBase
 	bool StartHardware() override;
 	bool StopHardware() override;
 	void Do_Work();
+
 	std::string MakeRequestURL(_eNetatmoType NetatmoType);
-	void GetWeatherDetails();
+
+        void GetWeatherDetails();
 	void GetHomecoachDetails();
         bool GetHomeDetails();
 	void GetHomesDataDetails();
 	void GetHomeStatusDetails();
 	void GetThermostatDetails();
-	bool ParseStationData(const std::string &sResult, bool bIsThermostat);
+
+        void Get_Measure();
+        void Get_Picture();
+        void Get_Events();
+
+        bool ParseStationData(const std::string &sResult, bool bIsThermostat);
 	bool ParseHomeData(const std::string &sResult);
 	bool ParseHomeStatus(const std::string &sResult);
+
 	bool SetAway(int idx, bool bIsAway);
 	bool SetSchedule(int scheduleId);
 
@@ -100,17 +102,43 @@ class CNetatmo : public CDomoticzHardwareBase
 
 	int m_ActHome;
 	std::string m_Home_ID;
-	std::string m_ThermostatName;
+        std::string m_Home_name;
+        std::string m_Place;
+
+        std::map<std::string, std::string> m_Camera_Name;
+        std::map<std::string, std::string> m_Camera_ID;
+        std::map<std::string, std::string> m_Smoke_Name;
+        std::map<std::string, std::string> m_Smoke_ID;
+
+        std::map<std::string, std::string> m_ThermostatName;
 	std::map<std::string, std::string> m_RoomNames;
 	std::map<std::string, int> m_RoomIDs;
+        std::map<std::string, std::string> m_Room;
 	std::map<std::string, std::string> m_ModuleNames;
 	std::map<std::string, int> m_ModuleIDs;
+        std::map<std::string, std::string> m_Persons;
+        std::map<std::string, std::string> m_PersonsNames;
+        std::map<std::string, int> m_PersonsIDs;
+        std::map<std::string, std::string> m_PersonUrl;
 	std::map<int, std::string> m_ScheduleNames;
 	std::map<int, std::string> m_ScheduleIDs;
 	int m_selectedScheduleID;
+        std::map<int, std::string> m_ZoneNames;
+        std::map<int, std::string> m_ZoneIDs;
+        std::map<int, std::string> m_ZoneTypes;
 
 	std::map<int, CBaroForecastCalculator> m_forecast_calculators;
 
 	int GetBatteryLevel(const std::string &ModuleType, int battery_percent);
 	bool ParseDashboard(const Json::Value &root, int DevIdx, int ID, const std::string &name, const std::string &ModuleType, int battery_percent, int rf_status);
+      public:
+        CNetatmo(int ID, const std::string &username, const std::string &password);
+        ~CNetatmo() override = default;
+        //
+        bool WriteToHardware(const char *, unsigned char) override;
+        void SetSetpoint(int idx, float temp);
+        bool SetProgramState(int idx, int newState);
+
+        void Get_Respons_API(const _eNetatmoType& NType, std::string& sResult, std::string& home_id , bool& bRet, Json::Value& root );
+        //
 };
