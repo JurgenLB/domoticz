d6f417b979bd45b47ac80267559e710223b09110
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-03-30 16:09:59 +0200

    Update HTTPClient.cpp

--- httpclient/HTTPClient.cpp
+++ httpclient/HTTPClient.cpp
@@ -325,7 +325,7 @@ bool HTTPClient::POSTBinary(const std::string &url, const std::string &postdata,
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response);
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 		curl_easy_setopt(curl, CURLOPT_POST, 1);
-		_log.Debug("...CURL URL %s", url.c_str());
+		_log.Debug(DEBUG_HARDWARE, "...CURL URL %s", url.c_str());
 		for (const auto& header : vHeaderData)
 		{
 			_log.Debug(DEBUG_HARDWARE, "...CURL vHeaderData %s", header.c_str());
