532619f37688a4244fbafcd834989e50cdb57941
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-03-30 16:36:49 +0200

    Update HTTPClient ExtraHeaders

--- httpclient/HTTPClient.cpp
+++ httpclient/HTTPClient.cpp
@@ -325,10 +325,15 @@ bool HTTPClient::POSTBinary(const std::string &url, const std::string &postdata,
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response);
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 		curl_easy_setopt(curl, CURLOPT_POST, 1);
-		_log.Debug(DEBUG_HARDWARE, "...CURL URL %s", url.c_str());
+		_log.Debug(DEBUG_HARDWARE, "...CURL URL  %s", url.c_str());
+		_log.Debug(DEBUG_HARDWARE, "...CURL POST %s", postdata.c_str());
 		for (const auto& header : vHeaderData)
 		{
 			_log.Debug(DEBUG_HARDWARE, "...CURL vHeaderData %s", header.c_str());
+		}
+		for (const auto& header : ExtraHeaders)
+		{
+			_log.Debug(DEBUG_HARDWARE, "...CURL ExtraHeaders %s", header.c_str());
 		}
 		
 		struct curl_slist *headers = nullptr;
