a5adbcaad4451230c5af4bc73df1e8e6f0f9554f
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-12-07 23:31:48 +0100

    Refactor maps Thermostat

--- hardware/Netatmo.h
+++ hardware/Netatmo.h
@@ -144,9 +144,9 @@ class CNetatmo : public CDomoticzHardwareBase
 	std::map<std::string, std::string> m_RoomNames;
 	std::map<std::string, std::string> m_Types;
 	std::map<std::string, Json::Value> m_Room;
-	std::map<std::string, std::string> m_Room_mode;
-	std::map<std::string, std::string> m_Room_setpoint;
-	std::map<std::string, std::string> m_Room_Temp;
+	std::map<std::string, std::map<std::string, int>> m_Room_combi;
+	std::map<std::string, std::map<std::string, std::string>> m_Room_setpoint;
+	std::map<std::string, std::map<std::string, std::string>> m_Room_HardwareID;
 	std::map<std::string, std::string> m_RoomIDs;
 	std::map<std::string, std::string> m_Module_category;
 	std::map<std::string, std::string> m_Device_types;
