3c8ded90081bf4d711727f9214074bf9e8260add
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-12-28 19:56:24 +0100

    Update pTypeFan to support FANEXT

--- hardware/RFXBase.cpp
+++ hardware/RFXBase.cpp
@@ -81,7 +81,7 @@ bool CRFXBase::CheckValidRFXData(const uint8_t *pData)
 	case pTypeChime:
 		return (pLen >= 0x07);
 	case pTypeFan:
-		return (pLen == 0x08 || pLen == 0x11);
+		return (pLen == 0x08 || pLen == 0x11 || pLen == 0x31);
 	case pTypeCurtain:
 		return (pLen == 0x07);
 	case pTypeBlinds:
