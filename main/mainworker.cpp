d5b047baa0370faa643474c9bd4722828f5819bc
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-12-28 20:22:03 +0100

    Orcon device subtype handling
    Use Destination ID for Orcon Device and add Debug information

--- main/mainworker.cpp
+++ main/mainworker.cpp
@@ -5688,7 +5688,17 @@ void MainWorker::decode_Fan(const CDomoticzHardwareBase* pHardware, const tRBUF*
 	char szTmp[100];
 	uint8_t devType = pTypeFan;
 	uint8_t subType = pResponse->FAN.subtype;
-	sprintf(szTmp, "%02X%02X%02X", pResponse->FAN.id1, pResponse->FAN.id2, pResponse->FAN.id3);
+
+	//      Make a selectorswitch for Orcon Device based on Destination ID
+	if (pResponse->ICMND.subtype == 0x0C)
+	{
+		_log.Debug(DEBUG_HARDWARE, "Subtype Orcon detected");
+		sprintf(szTmp, "%02X%02X%02X", pResponse->FAN2.did1, pResponse->FAN2.did2, pResponse->FAN2.did3);
+		//"CustomImage", 8 FAN
+	}
+	else
+		sprintf(szTmp, "%02X%02X%02X", pResponse->FAN.id1, pResponse->FAN.id2, pResponse->FAN.id3);
+
 	std::string ID = szTmp;
 	uint8_t Unit = 0;
 	uint8_t cmnd = pResponse->FAN.cmnd;
@@ -5803,7 +5813,74 @@ void MainWorker::decode_Fan(const CDomoticzHardwareBase* pHardware, const tRBUF*
 			break;
 		case sTypeOrcon:
 			WriteMessage("subtype       = Orcon");
-			break;
+			sprintf(szTmp, "ID            = %02X%02X%02X", pResponse->FAN2.id1, pResponse->FAN2.id2, pResponse->FAN2.id3);
+			WriteMessage(szTmp);
+			sprintf(szTmp, "Destination   = %02X%02X%02X", pResponse->FAN2.did1, pResponse->FAN2.did2, pResponse->FAN2.did3);
+			WriteMessage(szTmp);
+			WriteMessage("Command       = ", false);
+			switch (pResponse->FAN2.cmnd)
+			{
+				case fan_Orconlow:
+					WriteMessage("Low");
+					break;
+				case fan_Orconmedium:
+					WriteMessage("Medium");
+					break;
+				case fan_Orconhigh:
+					WriteMessage("High");
+					break;
+				case fan_Orcontimer1:
+					WriteMessage("Timer 1");
+					break;
+				case fan_Orcontimer2:
+					WriteMessage("Timer 2");
+					break;
+				case fan_Orcontimer3:
+					WriteMessage("Timer 3");
+					break;
+				case fan_Orconauto:
+					WriteMessage("Auto");
+					break;
+				case fan_Orconaway:
+					WriteMessage("Away");
+					break;
+				case fan_Orconjoin:
+					WriteMessage("Join");
+					break;
+				case fan_Orconleave:
+					WriteMessage("Leave");
+					break;
+				case fan_Orconstate:
+					WriteMessage("State");
+					break;
+				case fan_Orcontemp:
+					WriteMessage("Temperature");
+					break;
+				case fan_Orconco2:
+					WriteMessage("CO2");
+					break;
+				case fan_Orconbattery:
+					WriteMessage("Battery");
+					break;
+				case fan_Orconfilter:
+					WriteMessage("Filter");
+					break;
+				case fan_Orconpresence:
+					WriteMessage("Presence");
+					break;
+				case fan_Orconspeed:
+					WriteMessage("Speed");
+					break;
+				case fan_Orconstatus:
+					WriteMessage("Status");
+					break;
+				default:
+					WriteMessage("UNKNOWN");
+					break;
+				}
+
+				break;
+			}
 		case sTypeIthoHRU400:
 			WriteMessage("subtype       = Itho HRU400");
 			break;
